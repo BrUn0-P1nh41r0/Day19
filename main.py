09b2b205de7369be05b03962ac95faf0bc53d4e4
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-08 19:26:53 +0000

    Final project turtle race game

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
+import random
+
 from turtle import Turtle, Screen
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=1000, height=700)
+user_bet = screen.textinput(title= "Make your bet", prompt="Which turtle will win the race? Enter a color:")
+colors = ["red", "blue", "orange", "yellow", "green", "purple", "AliceBlue", "aquamarine", "brown", "DarkBlue", "cyan"]
+y_position = [-330, -264, -198, -132, -66, 0, 66, 132, 198, 264, 330]
+all_turtles = []
 
-def move_forwards():
-    tim.forward(10)
+for turtle_index in range(0,11):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-480, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
 
-def move_backwards():
-    tim.backward(10)
+if user_bet:
+    is_race_on = True
 
-def rotate_counter():
-    tim.left(10)
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 480:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-def rotate_clockwise():
-    tim.right(10)
 
-def clear_drawing():
-    tim.reset()
-
-screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=rotate_counter)
-screen.onkey(key="d", fun=rotate_clockwise)
-screen.onkey(key="c", fun=clear_drawing)
 screen.exitonclick()
